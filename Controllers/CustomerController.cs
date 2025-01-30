22241a0f172172c9f50d9b8bda1f9d7fb634a5f9
nitinpatel028 <137648574+nittiinn@users.noreply.github.com>
2025-01-30 15:34:29 +0530

    Add files via upload

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewMVCApplication.Data.Models;
+using NewMVCApplication.Entities;
+using NewMVCApplication.Models;
+
+namespace NewMVCApplication.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CustomerDbContext _context;
+
+        public CustomerController(CustomerDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(customer);
+        }
+    }
+}
